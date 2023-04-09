f95ae701b5d8f6806e4965cf2decd320bf21aa5b
Umit Yildirim <umity@uia.no>
2023-04-10 01:23:20 +0200

    endring2

--- main.go
+++ main.go
@@ -27,15 +27,13 @@ func main() {
 
 		case "convert":
 			fmt.Println("Konverterer alle målingene gitt i grader Celsius til grader Fahrenheit...")
-			// funksjon som gjør åpner fil, leser linjer, gjør endringer og lagrer nye linjer i en ny fil
 			yr.ConvertTemperature()
 
 		case "average":
 			fmt.Println("Gjennomsnitt-kalkulator")
 
 			for {
-				// funksjon som deler opp datalinjene for å single ut det siste tallet, som er temperatur i celsius.
-				// Funksjonen tar så alle de siste tallene i filen og regner ut gjennomnsnitt i enten celsius eller fahr.
+
 				yr.AverageTemperature()
 
 				var input2 string
